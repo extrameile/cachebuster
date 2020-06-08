87a40c00ae7c275bc39b9e949ec2436ac4faa219
Alexander Opitz <opitz@extrameile.agency>
2020-06-08 09:17:13 +0200

    [TASK] Update dependency to 8/9/10LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,20 +4,18 @@
  * Extension Manager/Repository config file for ext: "cachebuster"
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF['cachebuster'] = [
     'title' => 'CacheBuster',
     'description' => 'Adds a cachebuster to files referenced via FAL and Fluids f:uri.resource Viewhelper',
     'category' => 'fe',
-    'author' => 'Christoph Lehmann',
-    'author_email' => 'post@christophlehmann.eu',
+    'author' => 'Christoph Lehmann, Alexander Opitz',
+    'author_email' => 'post@christophlehmann.eu, opitz@extrameile-gehen.de',
     'state' => 'beta',
     'clearCacheOnLoad' => 1,
-    'version' => '0.1.0',
-    'constraints' => array(
-        'depends' => array(
-            'typo3' => '6.2.0-8.99.99',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    ),
-);
+    'version' => '0.2.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.5.0-10.4.99',
+        ],
+    ],
+];
